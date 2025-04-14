f9556d09bac5f949519df35d4d43a00f1b32ef25
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 00:27:55 +0300

    Update waifu.js

--- Coolkid/Anime/waifu.js
+++ Coolkid/Anime/waifu.js
@@ -1,1 +1,18 @@
+const axios = require('axios');
 
+module.exports = {
+  name: 'waifu',
+  category: 'anime',
+  description: 'Sends a random SFW waifu image.',
+  run: async (ctx) => {
+    try {
+      const res = await axios.get('https://api.waifu.pics/sfw/waifu');
+      const imageUrl = res.data.url;
+
+      await ctx.replyWithPhoto({ url: imageUrl }, { caption: 'Here is your waifu!' });
+    } catch (err) {
+      console.error('Waifu command error:', err);
+      ctx.reply('Failed to fetch waifu. Try again later.');
+    }
+  }
+};
