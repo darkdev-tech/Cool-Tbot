c873d242caf83e9744d9cfea50e23f051c7175bc
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 00:36:19 +0300

    Update dev.js

--- Coolkid/General/dev.js
+++ Coolkid/General/dev.js
@@ -1,1 +1,16 @@
+module.exports = {
+  name: 'dev',
+  category: 'general',
+  description: 'Displays information about the bot developer.',
+  run: async (ctx) => {
+    const message = `
+*COOL_KID Bot Developer Info*
+👨‍💻 Developer: Cool_kid
+🛠 Tech: https://whatsapp.com/channel/0029VarDt9t30LKL1SoYXy26
+📦 Source: (https://github.com/darkdev-tech/Cool-Tbot)
+💬 Need help? Contact: @WEB_X ꪹ
+    `.trim();
 
+    await ctx.replyWithMarkdown(message);
+  }
+};
